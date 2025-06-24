7cf45f0af1ba5d067acfe4b1b7721dfc159d60b1
DeKuyash <hadievemil19@gmail.com>
2025-06-24 22:18:11 +0500

    Delete cuffs directory

--- cuffs/lua/server/server.lua
+++
@@ -1,9 +0,0 @@
-
-
-
-hook.Add("PlayerSwitchWeapon", "PreventWeaponSwitch", function(ply, oldWeapon, newWeapon)
-    if ply:HasWeapon('cuffed') == true then
-        ply:SetActiveWeapon(cuffed)
-        return true 
-    end
-end)
